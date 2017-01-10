64a00ea0df10ae58f6c410795c631d82615bb4b0
NLHawkins <nathanlhawkins@gmail.com>
2017-01-10 12:35:27 -0500

    Tues Morning Notes update1

--- Program.cs
+++ Program.cs
@@ -10,6 +10,28 @@ namespace Week1
     {
         static void Main(string[] args)
         {
+            string[] board =
+            {
+                "_","_","_","_","_","_","_","_","_",
+            };
+            int counter = 0;
+            foreach(string place in board)
+            {
+                if (counter== 3)
+                {
+                    Console.WriteLine("");
+                    Console.Write("_ " + place);
+                    counter = 1;
+                
+                }
+                else
+                {
+                    Console.Write("_" + place);
+                    counter++;
+                }
+            }
+
+
             var myString = "Joel";
             var cleanedString = myString.Replace('o','0');
 
@@ -28,7 +50,43 @@ namespace Week1
                 ages[i] = age;
                 // or 
                 // ages[i] = int.Parse(console.ReadLine());
+
+                if (i == 2)
+                {
+                    Console.WriteLine("Magic Johnson");
+                }
+
+                else if (i== 3)
+                {
+                    Console.WriteLine("Unlucky Person");
+                }
             }
+            //to list each age
+            foreach(var age in ages)
+            {
+                string readableDrinkAge;
+                bool isLegalToDrink = age >= 21;
+                if (isLegalToDrink)
+                {
+                    readableDrinkAge = "Yes";
+                }
+                else
+                {
+                    readableDrinkAge = "No";
+                }
+             
+                //shown in ternary style
+
+                
+                
+                Console.WriteLine($"{age} is legal to drink? {readableDrinkAge}");
+                   
+            }
+            //method max on array(ages)
+            Console.WriteLine("--------");
+            Console.WriteLine(ages.Max());
+            Console.WriteLine(ages.Min());
+            Console.WriteLine(ages.Average());
 
             string[] myArray = new string[9];
             myArray[0] = "X";
