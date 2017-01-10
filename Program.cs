16560811f5c1c15df0436ed5826ff0be9b914c6b
NLHawkins <nathanlhawkins@gmail.com>
2017-01-10 10:50:01 -0500

    tues morn1

--- Program.cs
+++ Program.cs
@@ -10,6 +10,29 @@ namespace Week1
     {
         static void Main(string[] args)
         {
+            var myString = "Joel";
+            var cleanedString = myString.Replace('o','0');
+
+
+            //dynamic code which represents changing size
+            //int.Parse used to parse interger to string
+            int classSize = int.Parse(Console.ReadLine());
+            int[] ages = new int[classSize];
+            
+            for (int i = 0; i < classSize; i++)
+            {
+                Console.WriteLine($"Age of {i} Student?");
+                //$ allows for string to be interpolated
+                //{i} "called accumulator"
+                int age = int.Parse(Console.ReadLine());
+                ages[i] = age;
+                // or 
+                // ages[i] = int.Parse(console.ReadLine());
+            }
+
+            string[] myArray = new string[9];
+            myArray[0] = "X";
+            Console.WriteLine(myArray);
         }
     }
 }
